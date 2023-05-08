3cb49820ccaf0d31e2007e381b0462ec2d77a626
Khusan <khusanabdukarimov007@gmail.com>
2023-05-08 16:17:20 +0500

    done

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        int s = (int) minutes;
+
+        System.out.println((float) s * 60 + 30);
     }
 }
